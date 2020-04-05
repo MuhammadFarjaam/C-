ea5d0c0138d4c1e6e1a0cea657f79c86c470dd33
MuhammadFarjaam <53135466+MuhammadFarjaam@users.noreply.github.com>
2020-04-05 07:40:52 +0500

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,20 @@
+/*#include <iostream>
+using namespace std;
+int main(){
+
+	int a, b, i;
+	cout << "Program To Fine Square and Cube Of Numbers ";
+	cout << "\n\nEnter Uper Limit" << endl;
+	cin >> a;
+	cout << "\nEnter Lower Limit" << endl;
+	cin >> b;
+	cout << "-------------------------------" << endl;
+	cout << "\t|  Square" << "\t|  Cube" << endl;
+	// for loop
+	for (i = a; i <= b; i++){
+
+		cout << i << "\t|  " << i*i;
+		cout << "\t\t|  " << i*i*i << endl;
+	}
+
+}*/
